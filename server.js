bdf33628e4065e38249ceb462b65ec93fd7baf6f
Anon <anon@Anons-MacBook-Pro.local>
2024-09-04 15:20:39 +0200

    Message décrivant les changems

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post('/create-transaction', async (req, res) => {
 
         console.log("Transaction créée avec succès avec recentBlockhash.");
 
-        // Sérialiser la transaction sans la signer
+        // Sérialiser la transaction pour le client
         const serializedTransaction = transaction.serializeMessage().toString('base64');
         res.json({ transaction: serializedTransaction });
     } catch (error) {
